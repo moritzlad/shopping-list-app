7ba2b713bea75bddfe1141c6b74ae9b318b30668
Moritz <moritz.ladenburger@tum.de>
2025-06-04 18:45:18 +0200

    Rearange UI

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,19 @@ function App() {
     setItems([...items, item])
   }
 
-
   return (
-    <div className="p-4 max-w-md mx-auto">
-      <Header />
-      <ItemForm onAddItem={addItem}/>
-      <ItemList items={items}/>
+    <div className="min-h-screen">
+      <div className="fixed top-0 left-0 right-0 z-10 text-center py-4 bg-white shadow-sm">
+        <Header />
+      </div>
+      <div className="w-screen pt-32 pb-20 px-4">
+        <ItemList items={items}/>
+      </div>
+      <div className="fixed bottom-0 left-0 right-0 z-10 p-4 bg-white shadow-lg border-t">
+        <div className="max-w-md mx-auto">
+          <ItemForm onAddItem={addItem}/>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
       <header className="mb-4">
-        <h1 className="text-3xl font-bold text-center text-blue-600">Shopping List</h1>
+        <h1 className="text-2xl font-bold text-center">Shopping List</h1>
       </header>
     )
   }
